f32baac27aa06554894e1452357f554916c9927a
Rodrigo Kraus <rodrikraus10@hotmail.com>
2025-05-15 20:30:53 -0300

    Annotation quickfix

--- src/main/java/com/Manel_Backend/Dtos/PurchaseRequest.java
+++ src/main/java/com/Manel_Backend/Dtos/PurchaseRequest.java
@@ -2,11 +2,13 @@ package com.Manel_Backend.Dtos;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class PurchaseRequest {
