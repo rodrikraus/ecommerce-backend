d1ae7cac6e8f8ba5f216282b46c09d83652c3c92
Rodrigo Kraus <rodrikraus10@hotmail.com>
2025-06-02 16:58:10 -0300

    Temporarily enabled CORS for every endpoint (while in development), will disable when adding security features.

---
+++ src/main/java/com/Manel_Backend/Config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.Manel_Backend.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//Habilitar el CORS para permitir solicitudes desde el frontend
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
