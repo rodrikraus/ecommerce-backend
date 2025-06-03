1bf21ef9c15c88b557a6662e056566d89a93d262
Rodrigo Kraus <rodrikraus10@hotmail.com>
2025-06-02 21:31:11 -0300

    Searchable tags fixed.

--- src/main/java/com/Manel_Backend/Models/User.java
+++ src/main/java/com/Manel_Backend/Models/User.java
@@ -2,7 +2,6 @@ package com.Manel_Backend.Models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
