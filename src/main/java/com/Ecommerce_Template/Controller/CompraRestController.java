94d263b63f850ed7d6c095eb428f5a9a8974b167
rodrikraus <rodrikraus@hotmail.com>
2025-06-07 22:33:47 -0300

    Prices now entirely calculated on the backend for security measures.

--- src/main/java/com/Ecommerce_Template/Controller/CompraRestController.java
+++ src/main/java/com/Ecommerce_Template/Controller/CompraRestController.java
@@ -1,5 +1,7 @@
 package com.Ecommerce_Template.Controller;
 
+import java.math.BigDecimal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,17 +9,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Ecommerce_Template.Models.Compra;
+import com.Ecommerce_Template.Models.CompraItem;
 import com.Ecommerce_Template.Service.CompraService;
+import com.Ecommerce_Template.Service.ProductService;
 
 @RestController
 @RequestMapping("/api/compras")
 public class CompraRestController {
     @Autowired
     private CompraService compraService;
+    @Autowired
+    private ProductService productService;
 
     @PostMapping
     public String agregarCompra(@RequestBody Compra c) {
-        compraService.crearCompra(c);
+        Compra compra = new Compra();
+        compra.setEmail(c.getEmail());
+        compra.setFechaCompra(c.getFechaCompra());
+        compra.setId(c.getId());
+        compra.setItems(c.getItems());
+        compra.setShippingAddress(c.getShippingAddress());
+        BigDecimal total = new BigDecimal("0");
+        for(CompraItem i : c.getItems()) {
+            BigDecimal precioProducto = productService.buscarProducto(i.getProduct().getId()).getPrice();
+            total = total.add(precioProducto);
+        }
+        compra.setTotalAmount(total);
+        compraService.crearCompra(compra);
         return "Compra creada con éxito!";
     }
 }
